adfc16be23225f5a28cc2039dc18029c3d2e599f
etudiant <etudiant@ubuntu.mtl.sfl>
2019-10-29 10:06:26 -0400

    Fix 2 bugs

--- CH10/pdb_tutorial/dicegame/utils.py
+++ CH10/pdb_tutorial/dicegame/utils.py
@@ -8,4 +8,4 @@ def i_just_throw_an_exception():
         value += 1
 
     some_value = "I don't know what you were expecting"
-    raise UnnecessaryError("You actually called this function...")
+    #raise UnnecessaryError("You actually called this function...")
