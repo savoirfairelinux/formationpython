dc6493323bfd24f41401fb400ab95bf1333fb5fb
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2019-10-29 15:44:06 -0400

    chore(SAM-0001): Add more tests

--- CH10/pdb_tutorial/tests/test_simple.py
+++ CH10/pdb_tutorial/tests/test_simple.py
@@ -1,15 +1,32 @@
 import unittest
-from dicegame.die import Die
+from CH10.pdb_tutorial.dicegame.die import Die
 
+scope = 2
 
 class TestStringMethods(unittest.TestCase):
 
     def setup(self):
-        die = Die()
-        die.roll()
+        self.die = Die()
+        self.die.roll()
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_scope(self):
+        myDic = {
+          'something': 1,
+          'something_else': 'haha'
+        }
+
+        print(myDic[])
+        # scope = 2
+        # def myFunc():
+        #     scope = 5
+        # myFunc()
+        # if True:
+        #     scope = 6
+        self.assertEqual(scope, 5)
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
