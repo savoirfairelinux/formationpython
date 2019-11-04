70ca64719e6283e9a488ed0984fb6914400c3312
mlevkovsky <mikhail.levkovsky@ssense.com>
2019-11-04 11:00:11 -0500

    pair impair exercise

--- CH02/ch02_exercises/ch02_ex1.py
+++ CH02/ch02_exercises/ch02_ex1.py
@@ -1,6 +1,7 @@
-def pair_impair(odd_even_list):
-    for i in odd_even_list:
-        if int(i) % 2 == 0:
-            print("pair")
-            continue
-        print("impair")
+odd_even_list = ['1', '2', '3', '4']
+
+for i in odd_even_list:
+    if int(i) % 2 == 0:
+        print("pair")
+        continue
+    print("impair")
