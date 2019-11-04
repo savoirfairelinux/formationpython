255136a1002c7f7753f22e9922f0185fb3a58539
Marc Renaud <marc.renaud@ssense.com>
2019-11-04 11:34:05 -0500

    exercise 2

---
+++ CH02/ch02_exercises/ch02_ex3.py
@@ -0,0 +1,9 @@
+
+def maj_inverser(stringValue):
+    newString = ""
+    for value in stringValue[::-1]
+        newString += value
+    newString.capitalize()
+    return newString
+
+print(maj_inverser("abcdefg"))
