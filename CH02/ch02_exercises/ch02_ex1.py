0deaeae608b43de980081dfd5dba8311f438692a
j-philippe.lingrand <jean-philippe.lingrand@ssense.com>
2019-11-04 11:08:03 -0500

    add script input parsing

--- CH02/ch02_exercises/ch02_ex1.py
+++ CH02/ch02_exercises/ch02_ex1.py
@@ -1,4 +1,10 @@
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+import sys
+print (sys.argv)
+
+input_array = sys.argv[1:]
+
+for num in input_array:
+    if (int(num) % 2) == 0:
+       print("{0} is Even".format(num))
+    else:
+       print("{0} is Odd".format(num))
