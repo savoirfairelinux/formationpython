2574f9f8c274f95756848aa604d5c94c06332648
Marc Renaud <marc.renaud@ssense.com>
2019-11-04 12:14:21 -0500

    exercise 1

---
+++ CH01/ch01_exercises/ch01_ex1.py
@@ -0,0 +1,13 @@
+
+def get_grade(value):
+    integerValue = int(value)
+    if integerValue > 90: return "A"
+    if integerValue > 80: return "B"
+    if integerValue > 70: return "C"
+    if integerValue > 60: return "D"
+    else: return "F"
+
+def print_grade(value):
+    print(get_grade(value))
+
+print_grade(80)
