3bd7de907f6ea8fb098b2b023839063f8ed1a644
Marc Renaud <marc.renaud@ssense.com>
2019-11-04 13:43:23 -0500

    asdf

--- CH01/ch01_exercises/ch01_ex1.py
+++ CH01/ch01_exercises/ch01_ex1.py
@@ -4,10 +4,10 @@ def print_grade(value):
 
 def get_grade(value):
     integerValue = int(value)
-    if integerValue > 90: return "A"
-    if integerValue > 80: return "B"
-    if integerValue > 70: return "C"
-    if integerValue > 60: return "D"
+    if integerValue >= 90: return "A"
+    if integerValue >= 80: return "B"
+    if integerValue >= 70: return "C"
+    if integerValue >= 60: return "D"
     else: return "F"
 
 print_grade(80)
