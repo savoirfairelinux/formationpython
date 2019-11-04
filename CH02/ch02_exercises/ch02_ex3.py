a4d43a8913f26bf4d18d7128519de95110a3121d
Marc Renaud <marc.renaud@ssense.com>
2019-11-04 11:36:05 -0500

    fixed

--- CH02/ch02_exercises/ch02_ex3.py
+++ CH02/ch02_exercises/ch02_ex3.py
@@ -1,9 +1,10 @@
 
 def maj_inverser(stringValue):
     newString = ""
-    for value in stringValue[::-1]
-        newString += value
-    newString.capitalize()
+    for value in stringValue[::-1]:
+        character = value = value.capitalize()
+        character.capitalize()
+        newString += character
     return newString
 
 print(maj_inverser("abcdefg"))
