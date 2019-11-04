62106b65aa5b2a9ef4d173824c95480592fa3338
gckc <20653046+gckc@users.noreply.github.com>
2019-11-04 15:01:06 -0500

    hoho

---
+++ CH05/ch05_exercises/ch05_ex1.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self, other_name):
+        return "Hi {0}, my name is {1}".format(other_name, self.name)
+
+x = Person('Tom Cruise')
+print(x.greet('Goose'))
