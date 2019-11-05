f0862380fc30b063117ab93da048247f9bc309f4
j-philippe.lingrand <jean-philippe.lingrand@ssense.com>
2019-11-05 16:23:59 -0500

    add test on api

--- CH09/monApp/tests/test_app.py
+++ CH09/monApp/tests/test_app.py
@@ -3,19 +3,20 @@ from falcon import testing
 import msgpack
 import pytest
 from unittest.mock import patch
-from monApp import ssenseProductClass
-
-from monApp import app
+from ..monApp.app import ssenseProductClass
+from ..monApp.app import api
+import json
+import pdb
 
 @pytest.fixture
 def client():
-    return testing.TestClient(app.api)
+    return testing.TestClient(api)
 
 
 # pytest will inject the object returned by the "client" function
 # as an additional parameter.
-@patch(ssenseProductClass, 'on_get')
-def test_list_images(client):
+# @patch(ssenseProductClass, 'on_get')
+def test_400(client):
     content = {
         'name': 'Black Ruben Coat',
         'brand': 'Nudie Jeans',
@@ -25,8 +26,58 @@ def test_list_images(client):
         'image': 'https://img.ssensemedia.com/images//192078M176001_1/nudie-jeans-black-ruben-coat.jpg'
     }
 
-    response = client.simulate_get('/getProduct')
-    result_doc = msgpack.unpackb(response.content, raw=False)
+    response = client.simulate_get('/getProducts')
 
-    assert result_doc == content
-    assert response.status == falcon.HTTP_OK
+    parsed_response = json.loads(response.content)
+
+    assert parsed_response == {
+        'title': "Bad request",
+        'description': "Request content type not allowed"
+    }
+
+def test_OK(client):
+    content = {
+        'name': 'Black Ruben Coat',
+        'brand': 'Nudie Jeans',
+        'price': '650',
+        'currency': 'CAD',
+        'code': '192078M176001',
+        'image': 'https://img.ssensemedia.com/images//192078M176001_1/nudie-jeans-black-ruben-coat.jpg'
+    }
+
+    response = client.simulate_get('/getProducts', headers={
+        'content-type': 'application/json'
+    })
+
+    parsed_response = json.loads(response.content)
+
+    assert len(parsed_response.keys()) == 13
+
+
+    #     assert response.content == "{"title: "Bad request",
+    # description: "Request content type not allowed"
+    # }"
+
+    # result_doc = msgpack.unpackb(response.content, raw=False)
+    #
+    # assert result_doc == content
+    # assert response.status == falcon.HTTP_OK
+
+# pytest will inject the object returned by the "client" function
+# as an additional parameter.
+# @patch(ssenseProductClass, 'on_get')
+# def test_content_type(client):
+#     content = {
+#         'name': 'Black Ruben Coat',
+#         'brand': 'Nudie Jeans',
+#         'price': '650',
+#         'currency': 'CAD',
+#         'code': '192078M176001',
+#         'image': 'https://img.ssensemedia.com/images//192078M176001_1/nudie-jeans-black-ruben-coat.jpg'
+#     }
+#
+#     response = client.simulate_get('/getProduct')
+#     result_doc = msgpack.unpackb(response.content, raw=False)
+#
+#     assert result_doc == content
+#     assert response.status == falcon.HTTP_OK
