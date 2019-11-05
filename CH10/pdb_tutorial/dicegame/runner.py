12f133c07c6cb6e1af9ca7881507ab11fcd2359f
Simone Nespoli <simone.nespoli@ssense.com>
2019-11-05 10:49:32 -0500

    fixed bugs

--- CH10/pdb_tutorial/dicegame/runner.py
+++ CH10/pdb_tutorial/dicegame/runner.py
@@ -1,11 +1,15 @@
+import ipdb
 from .die import Die
 from .utils import i_just_throw_an_exception
 
 class GameRunner:
 
     def __init__(self):
-        self.dice = Die.create_dice(5)
         self.reset()
+        self.roll()
+
+    def roll(self):
+        self.dice = Die.create_dice(2)
 
     def reset(self):
         self.round = 1
@@ -15,17 +19,16 @@ class GameRunner:
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value
         return total
 
     @classmethod
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
+        runner = cls()
         while True:
-            runner = cls()
-
+            runner.roll()
             print("Round {}\n".format(runner.round))
 
             for die in runner.dice:
@@ -37,24 +40,22 @@ class GameRunner:
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
-                c += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
-                print("Like seriously, how could you mess that up")
                 runner.loses += 1
-                c = 0
+
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
             runner.round += 1
 
-            if c == 6:
+            if runner.wins == 6:
                 print("You won... Congrats...")
                 print("The fact it took you so long is pretty sad")
                 break
 
             prompt = input("Would you like to play again?[Y/n]: ")
 
-            if prompt == 'y' or prompt == '':
+            if prompt.lower() == 'y' or prompt == '':
                 continue
             else:
-                i_just_throw_an_exception()
+                break
