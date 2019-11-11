d829aa67bd7d6eb90862d9994841f9a9376d330e
Kyle Khoury <kyle.khoury@ssense.com>
2019-11-11 15:54:35 -0500

    solution

--- CH10/pdb_tutorial/dicegame/runner.py
+++ CH10/pdb_tutorial/dicegame/runner.py
@@ -11,25 +11,33 @@ class GameRunner:
         self.round = 1
         self.wins = 0
         self.loses = 0
+        self.streak = 0
 
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value
         return total
+
+    def roll(self):
+        for die in self.dice:
+            die.roll()
+
+    def show(self):
+        for die in self.dice:
+            print(die.show())
 
     @classmethod
     def run(cls):
         # Probably counts wins or something.
         # Great variable name, 10/10.
-        c = 0
+        runner = cls()
         while True:
-            runner = cls()
+            runner.roll()
 
             print("Round {}\n".format(runner.round))
 
-            for die in runner.dice:
-                print(die.show())
+            runner.show()
 
             guess = input("Sigh. What is your guess?: ")
             guess = int(guess)
@@ -37,24 +45,23 @@ class GameRunner:
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
-                c += 1
+                runner.streak += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
                 runner.loses += 1
-                c = 0
+                runner.streak = 0
+
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
             runner.round += 1
 
-            if c == 6:
+            if runner.streak == 6:
                 print("You won... Congrats...")
                 print("The fact it took you so long is pretty sad")
                 break
 
             prompt = input("Would you like to play again?[Y/n]: ")
 
-            if prompt == 'y' or prompt == '':
-                continue
-            else:
-                i_just_throw_an_exception()
+            if prompt.lower() != 'y' and prompt != '':
+                break
