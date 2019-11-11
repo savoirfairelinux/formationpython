e8a5a2540ae41f034640744d900b17f5b1e10bbd
Kyle Khoury <kyle.khoury@ssense.com>
2019-11-11 11:29:59 -0500

    solution 2

--- CH02/ch02_exercises/ch02_ex4.py
+++ CH02/ch02_exercises/ch02_ex4.py
@@ -4,4 +4,7 @@ def compter_lettres(s: str):
         d[c] = (d[c] if c in d else 0) + 1
     return d
 
-print(compter_lettres('mississippi'))
+def compter_lettres2(s: str):
+    return {c: s.count(c) for c in s}
+
+print(compter_lettres2('mississippi'))
