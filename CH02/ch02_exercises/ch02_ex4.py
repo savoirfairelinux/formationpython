76cfe86ddfa96a7a2c44ae6ec1ade6e68a09ea1b
Kyle Khoury <kyle.khoury@ssense.com>
2019-11-11 11:28:07 -0500

    fix

--- CH02/ch02_exercises/ch02_ex4.py
+++ CH02/ch02_exercises/ch02_ex4.py
@@ -1,7 +1,7 @@
-def compter_letters(s: str):
+def compter_lettres(s: str):
     d = {}
     for c in s:
-        d[c] = (d[c] or 0) + 1
+        d[c] = (d[c] if c in d else 0) + 1
     return d
 
 print(compter_lettres('mississippi'))
