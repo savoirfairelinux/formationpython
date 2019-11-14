1056dc61d162f0bc7c5165fbe607f59299fb21b4
Kyle Khoury <kyle.khoury@ssense.com>
2019-11-13 20:33:51 -0500

    improve performance and accuracy

--- CH11/microservices-demo/project/pricing_engine.py
+++ CH11/microservices-demo/project/pricing_engine.py
@@ -2,56 +2,87 @@ import itertools
 import json
 import math
 import decimal
-
-from multiprocessing import Pool
-
+import requests
 import threading
 
-import requests
-
+from multiprocessing import Pool
 from collections import defaultdict
 
 from buyAndSellApi import DBConnect
 
 MY_STORE = 'MY_STORE'
 
-def floorNDigits(value, decimals):
-    with decimal.localcontext() as ctx:
-        d = decimal.Decimal(value)
-        ctx.rounding = decimal.ROUND_DOWN
-        return float(round(d, decimals))
+PRODUCT_NAME = 'Colourful'
+
+STORE_URLS = [
+    '192.168.0.141'
+]
+
+should_update_price = False
+
+def floor(value):
+   return math.floor(value * 100) / 100
 
 def get_expected_values(stores, msrp = 10):
     profits = defaultdict(int)
+    number_of_stores = len(stores)
 
-    if (len(stores) == 1):
-        return { stores[0][0]: 999999999 }
+    if (number_of_stores == 1):
+        return { stores[0][0]: stores[0][1] - msrp}
 
-    permutations = itertools.permutations(stores, min([3, len(stores)]))
+    # this will only work if the numbers of stores is at a maneagable level
+    # for 20 stores this would be 6840 permutations whereas for 100 stores this
+    # would be 970200 permutations.
+    number_of_stores_selected_by_customers = min([3, number_of_stores])
+    permutations = itertools.permutations(stores, )
 
-    count = 0
+    count_of_permutations = math.factorial(number_of_stores) / math.factorial(
+        number_of_stores - number_of_stores_selected_by_customers
+    ) if 0 < number_of_stores and number_of_stores <= number_of_stores_selected_by_customers else 0
 
     for permutation in permutations:
-        count += 1
         smallest = min(permutation, key=lambda x: x[1])
         winners = [store for store in permutation if store[1] == smallest[1]]
+
         for winner in winners:
+            # we divide our profit by the number of winners since in the case of
+            # a tie the sale will be awarded randomly to one of them.
             profits[winner[0]] += (winner[1] - msrp) / len(winners)
 
-    return {  name: floorNDigits(profits[name] / count, 2) for name, _ in stores }
+    return {  name: profits[name] / count_of_permutations for name, _ in stores }
 
-def process(args):
+def _run_get_expected_values(args):
     other_stores, my_price = args
-    return (my_price, get_expected_values([*other_stores, (MY_STORE, my_price)]))
+
+    return (
+        my_price,
+        get_expected_values([*other_stores, (MY_STORE, my_price)])
+    )
 
 def get_optimal_price(other_stores, msrp = 10):
-    if (not len(other_stores)): return 9999999
+    # if we are the only store we want to set our price as high as possible
+    if (not len(other_stores)): return 9999999999
 
-    my_prices = [(other_stores, max([msrp, floorNDigits(store[1], 2) - 0.01])) for store in other_stores]
+    # we will use different prices to get the EV (expected value) of each price
+    # we will try to price it at 1 cent less than everyone else
+    # and also we will try to price it the same as everyone else
+    # we then select the price that gives us the highest expected return
+    # it is important to also try the prices that others stores have already
+    # selected because they can sometimes have a higher EV than pricing it
+    # at 1 cent less.
+    # this could for sure be optimized, there are high guesses that we know
+    # for sure can never be profitable
+    my_prices = list(set(itertools.chain.from_iterable(
+        (floor(store[1]), max([msrp, floor(store[1]) - 0.01]))
+        for store in other_stores
+    )))
 
     pool = Pool(5)
 
-    results = pool.map(process, my_prices)
+    results = pool.map(
+        _run_get_expected_values,
+        [(other_stores, price) for price in my_prices]
+    )
 
     highest_ev = 0
     best_price = msrp
@@ -66,109 +97,67 @@ def get_optimal_price(other_stores, msrp = 10):
     print('Current prices:')
     print({name: f'{price}$' for name, price in other_stores})
     print(f'optimal price is: {best_price}$ with an EV of {highest_ev}$')
-    print('Current Expected Values:')
-    print({name: f'{price}$' for name, price in best_expected_values.items()})
+
+    if best_expected_values is not None:
+        print('Current Expected Values:')
+        print({name: f'{price}$' for name, price in best_expected_values.items()})
+
     print('\n==================\n')
 
     return best_price
 
-addresses = [
-    '172.16.17.108',
-    '172.16.17.151',
-    '172.16.17.106',
-    '172.16.17.169',
-    '172.16.17.203',
-    '172.16.17.133',
-    '172.16.17.166'
-]
-
-import aiohttp
-import asyncio
-
-async def fetch(name, session):
-    headers = {'User-Agent': '$BOSS$'}
-    try:
-        async with session.get(
-            f'http://{name}:8000/product/Colourful',
-            headers=headers,
-            ssl=False,
-            timeout=aiohttp.ClientTimeout(
-                total=None,
-                sock_connect=0.1,
-                sock_read=0.1
-            )
-        ) as response:
-            return (name, await response.json())
-    except Exception:
-        return None
-
-# async def main():
-#     connection = DBConnect('Colourful')
-
-#     async with aiohttp.ClientSession() as session:
-#         print('==================\n')
-#         tasks = [
-#             asyncio.ensure_future(fetch(url, session))
-#             for url in addresses
-#         ]
-
-#         responses = await asyncio.gather(*tasks)
-
-#         stores = [
-#             (response[0], response[1]['price'])
-#             for response in responses
-#             if response is not None
-#         ]
-
-#         new_price = get_optimal_price(stores)
-#         print(f'setting price to {new_price}')
-#         connection.setProductPrice(new_price)
-#         print('\n')
-
-#     return stores
-
-################# V2 #################
-
-def update_prices(name, stores, connection):
+def fetch_prices(name, stores):
     try:
         result = requests.get(
-            f'http://{name}:8000/product/Colourful',
+            f'http://{name}:8000/product/{PRODUCT_NAME}',
             timeout=100
         ).json()
     except Exception:
         return
+    on_price_update(stores, name, result['price'])
 
-    store_name = name
-    store_price = result['price']
+def on_price_update(stores, store_name, store_price):
+    global should_update_price
 
     if store_name not in stores or stores[store_name] != store_price:
         stores[store_name] = store_price
-        update_to_optimal_price(stores.copy(), connection)
+        should_update_price = True
 
-def update_to_optimal_price(stores, connection):
-    new_price = get_optimal_price(list(stores.items()))
-    print(f'setting price to {new_price}$')
-    connection.setProductPrice(new_price)
-    print('\n')
+def run_price_updater(stores, connection):
+    global should_update_price
+    while True:
+        if (not should_update_price): continue
+        should_update_price = False
+        # we copy the prices here
+        # so that anothre thread doesn't overwrite them while
+        # we are processing them
+        stores = stores.copy()
+        new_price = get_optimal_price(list(stores.items()))
+        print(f'setting price to {new_price}$')
+        connection.setProductPrice(new_price)
+        print('\n')
 
-def mainV2():
+def run_price_poller_for(name, stores):
+    while True: fetch_prices(name, stores)
+
+def main():
     connection = DBConnect('Colourful')
     stores = {}
 
-    def update_prices_for(name):
-        while True:
-            update_prices(name, stores, connection)
+    threads = [
+        threading.Thread(target=run_price_poller_for, args=(url, stores))
+        for url in STORE_URLS
+    ] + [threading.Thread(target=run_price_updater, args=(stores, connection))]
 
-    tasks = [
-        threading.Thread(target=update_prices_for, args=(url,))
-        for url in addresses
-    ]
+    for thread in threads:
+        thread.start()
 
-    for task in tasks:
-        task.start()
+    for thread in threads:
+        thread.join()
 
-    for task in tasks:
-        task.join()
+# print(get_optimal_price((('a', 10.05), ('b', 10.05), ('c', 10.05), ('d', 10.05), ('e', 10.04), ('f', 10.8), ('g', 10.07))))
+# print(get_expected_values((('a', 100), ('b', 100), ('c', 11.25), ('d', 10.05), ('e', 10.04), ('f', 10.8), ('g', 10.07))))
+# print(get_optimal_price((('a', 100), ('b', 100), ('c', 11.25), ('d', 10.05), ('e', 10.04), ('f', 10.8), ('g', 10.07))))
+# print(get_expected_values((('a', 10.70), ('b', 10.42), ('c', 10.29), ('d', 10.17), ('e', 10.23), ('f', 10.25), ('g', 10.69), ('MY_STORE', 10.16))))
 
-# asyncio.run(main())
-mainV2()
+main()
